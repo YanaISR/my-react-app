4c6059465a3c2f5fe19c7afcf87d8e1d92e199e0
Yana <miriel.yana@gmail.com>
2023-10-20 18:19:53 +0300

    Hw 'useContext' is uodated

---
+++ src/components/List.jsx
@@ -0,0 +1,18 @@
+import { useContext } from "react";
+import { Context } from "../App";
+
+
+const List = () => {
+    const tasks = useContext(Context);
+  return (
+    <>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task}>{task}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default List;
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Widgets from "./Widgets";
+
+const Tasks = () => {
+  return (
+    <>
+      <Widgets />
+    </>
+  );
+};
+
+export default Tasks;
